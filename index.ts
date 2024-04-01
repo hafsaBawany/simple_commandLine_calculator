e24cd27bc5dd1d074511ee3c7c952c391744090f
hafsa bawany <bawanyh4@gmail.com>
2024-04-02 01:17:18 +0500

    adding a simple commandline calculator

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+console.log("\n\t A simple calculator for your simple problems\n")
+
+const answer = await inquirer.prompt([
+    {message:"enter your first number" , type: "number", name :"firstNumber" },
+    {message:"enter your second number" , type: "number", name :"secondNumber" },
+    {message:"select one of the operators to perform operation" ,
+     type: "list",
+     name :"operator",
+     choices : ["addition" , "subtraction" , "multiplication" , "division" ],
+     },
+]);
+
+if (answer.operator === "addition"){
+    console.log(/*"your answer is "*/ + answer.firstNumber + answer.secondNumber);
+}
+else if  (answer.operator === "substraction"){
+    console.log(/*"your answer is "*/ + answer.firstNumber - answer.secondNumber);
+}
+else if  (answer.operator === "multiplication"){
+    console.log(/*"your answer is "*/ + answer.firstNumber * answer.secondNumber);
+}
+else if  (answer.operator === "division"){
+    console.log(/*"your answer is "*/ + answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("syntax error")
+};
